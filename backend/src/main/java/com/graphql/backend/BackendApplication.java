334fc94a7ea0da5f215f6141318c3e36827746d4
palesmariann <mariann.pales@gmail.com>
2021-10-26 16:38:00 +0200

    booklist

--- backend/src/main/java/com/graphql/backend/BackendApplication.java
+++ backend/src/main/java/com/graphql/backend/BackendApplication.java
@@ -1,40 +1,13 @@
 package com.graphql.backend;
 
-import graphql.ExecutionResult;
-import graphql.GraphQL;
-import graphql.schema.GraphQLSchema;
-import graphql.schema.StaticDataFetcher;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-
-//        SpringApplication.run(BackendApplication.class, args);
-        String schema = "type Query{hello: String}";
-
-        SchemaParser schemaParser = new SchemaParser();
-        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
-
-        RuntimeWiring runtimeWiring = newRuntimeWiring()
-                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
-                .build();
-
-        SchemaGenerator schemaGenerator = new SchemaGenerator();
-        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
-
-        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
-        ExecutionResult executionResult = build.execute("{hello}");
-
-        System.out.println(executionResult.getData().toString());
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
